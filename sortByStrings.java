11d1ddeb19059886cb60e1436cef06e1444e2929
2993ashwini <40080405+2993ashwini@users.noreply.github.com>
2018-06-09 00:31:54 -0400

    Update and rename index.md to sortByStrings.java

---
+++ sortByStrings.java
@@ -0,0 +1,11 @@
+public static sortByStrings(String s, String t){
+
+String newS = "";
+
+for(int i = 0; i < t.length; i++){
+for(int i = 0; i < t.length; i++){
+if(s[j] == s[t])
+newS = s[j];
+}
+}
+}
