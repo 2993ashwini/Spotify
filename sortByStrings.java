046741a53c40429e945fd262ff3e98f9bca5c31b
2993ashwini <40080405+2993ashwini@users.noreply.github.com>
2018-06-09 00:43:59 -0400

    Update sortByStrings.java

--- sortByStrings.java
+++ sortByStrings.java
@@ -1,11 +1,21 @@
 public static sortByStrings(String s, String t){
 
 String newS = "";
+int[] chars = new char[MAX_LENGTH];
+  
+  for(int i = 0; i < s.length; i++)
+  {
+    int n = (int)s.charAt(i);
+    chars[n]++;
+  }
+  
+  for(int j = 0; j <t.length; j++){
+   int n = (int)t.charAt(i);
+    while(chars[n] >=0){
+    newS = t.charAt(i);
+      chars[n]--;
+    }
+    
+  }
 
-for(int i = 0; i < t.length; i++){
-for(int i = 0; i < t.length; i++){
-if(s[j] == s[t])
-newS = s[j];
-}
-}
 }
